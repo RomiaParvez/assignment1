037e6c257590cda190ef82d06364a46be5315c65
RomiaParvez <romiaparvez@gmail.com>
2025-03-16 16:19:02 +0530

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,9 @@
+# Take user's first name and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate to form full name
+full_name = first_name + " " + last_name
+
+# Print a personalized greeting
+print(f"Hello, {full_name}! How are you today?.")
